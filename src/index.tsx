4c9ef0268317e18b6e1162549fdcc94f24b1e2c6
maxim <trifonov.max@mail.ru>
2025-03-14 22:34:56 +0300

    feature(core): added reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 
 import {App} from './app';
 
-import './style.css';
+import './styles/reset.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
