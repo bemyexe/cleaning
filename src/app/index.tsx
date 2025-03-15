0737b3c52460aee8de878a2951a9b48e5d6c10ca
maxim <trifonov.max@mail.ru>
2025-03-15 11:32:32 +0300

    feature(svg): added icons, add dev-deps svgr

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import Arrow from '../assets/arrow.svg?react';
+
+import './style.scss';
+
+export const App = () => {
+  return (
+    <div>
+      App
+      <Arrow className="test" />
+    </div>
+  );
+};
